b52ebe40024ea4fba7c594aede0003b484cf3c53
JuanMa <juanma.garrido@gmail.com>
2025-11-20 11:13:02 +0000

    Implement build files and update .gitignore
    - Added build output files: index.asset.php, index.js, style-index.css, and style-index-rtl.css for the Media Manager Plugin.
    - Updated .gitignore to exclude the build directory, ensuring cleaner version control.

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-core-data', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'd39bf11af856d38ba12a');
